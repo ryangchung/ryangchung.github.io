11577e6b8080f608a442d8f76f6c3d83b0452e56
Ryan C <87027981+ryangchung@users.noreply.github.com>
2024-11-14 22:53:11 -0500

    chore: update text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,13 +29,13 @@ function Home() {
   return (
     <div style={pageContainerStyle}>
       <div style={textContainerStyle}>
-        <p>Bonjour-Hi! I'm Ryan, a software developer interested in all matters infrastructure and communities, 2.5+ years of internship and contract experience.</p>
+        <p>Bonjour-Hi! I'm Ryan, a software developer interested in all matters infrastructure and communities, ammassing over 2.5 years of internship and contract experience.</p>
         <br />
         <p>Returning to <a href="https://rbc.com/" target="_blank">RBC</a> for the Winter 2025 term in the International Money Transfers team, so that 20+ million people and businesses can bank with confidence.</p>
         <br />
-        <p>I've also been fortunate to work with unique teams at <a href="https://rbc.com/" target="_blank">RBC</a> (online banking and payments), <a href="https://www.decipad.com/" target="_blank">Decipad</a> (pipelines and interpreters) and Digitera Interactive (native Android).</p>
+        <p>I've also been fortunate to work with unique teams at <a href="https://rbc.com/" target="_blank">RBC</a> (online banking and payments), <a href="https://www.decipad.com/" target="_blank">Decipad</a> (pipelines and interpreters) and Digitera Interactive (android).</p>
         <br />
-        <p>Currently studying Computer Science at Carleton University in Ottawa and giving back to the community as an executive in the 3,000-member <a href="https://ccss.carleton.ca/">Carleton Computer Science Society</a>, where I manage funding and open-source initiatives.</p>
+        <p>Currently studying Computer Science at Carleton University in Ottawa and serving as an executive in the 3,000-member <a href="https://ccss.carleton.ca/">Carleton Computer Science Society</a>, where I manage funding and open-source initiatives.</p>
         <br />
         <p style={textEmphasisStyle}>Open for internships starting May 2024.</p>
         <br />
